ba89dec69e85711b51a72334a4d9e910bbda42fa
teymuraliyev1 <118759224+teymuraliyev1@users.noreply.github.com>
2024-12-21 09:12:45 +0400

    message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "sonner";
+import Navbar from "./components/Navbar";
+import Home from "./home/Home";
+import DetailedRecipe from "./recipe/detailed-page";
+import MainPage from "./recipe/main-page";
+import Create from "./recipe/create/create";
+import Contact from "./contact/contact";
 
 function App() {
+  console.log("App");
+
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Toaster position="top-right" />
+        <Navbar />
+        <main className="container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/recipes" element={<MainPage />} />
+            <Route path="/recipes/create" element={<Create />} />
+            <Route path="/recipes/:id" element={<DetailedRecipe />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
